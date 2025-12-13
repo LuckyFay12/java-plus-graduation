37f806b441f55c11d5faaa7203bf0b7f5226e33c
Ekaterina <e.fayzulina@inbox.ru>
2025-12-13 14:27:12 +0700

    Spring cloud

--- stats/stats-client/src/main/java/ru/practicum/client/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/client/StatClient.java
@@ -2,55 +2,105 @@ package ru.practicum.client;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.ewm.dto.EndpointHit;
 import ru.practicum.ewm.dto.StatRequest;
 import ru.practicum.ewm.dto.ViewStatDto;
-import ru.practicum.ewm.exception.ApiError;
-import ru.practicum.utils.ResponseGenerator;
 
+import java.net.URI;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 
-@Component
 @Slf4j
-public class StatClient extends ResponseGenerator {
+@Component
+public class StatClient {
 
     private final RestClient restClient;
+    private final DiscoveryClient discoveryClient;
+    private final RetryTemplate retryTemplate;
 
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    @Value("${stat-svc-service.url}")
-    private String statServiceUrl;
+    @Value("${stats-service.name:stats-service}")  //в main-service
+    private String statsServiceId;
 
-    public StatClient(@Value("${stat-svc-service.url}") String statServiceUrl) {
-        restClient = RestClient.builder()
-                .baseUrl(statServiceUrl)
-                .build();
+    public StatClient(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+        this.restClient = RestClient.create();
+        this.retryTemplate = createRetryTemplate();
+    }
+
+    private RetryTemplate createRetryTemplate() {
+        RetryTemplate template = new RetryTemplate();
+        MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy(3);
+        template.setRetryPolicy(retryPolicy);
+
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(3000L);
+        template.setBackOffPolicy(backOffPolicy);
+
+        return template;
+    }
+
+    private ServiceInstance getStatsServiceInstance() {
+        return retryTemplate.execute(context -> {
+            log.debug("Попытка #{} получить экземпляр сервиса {}",
+                    context.getRetryCount() + 1, statsServiceId);
+
+            List<ServiceInstance> instances = discoveryClient.getInstances(statsServiceId);
+            if (instances == null || instances.isEmpty()) {
+                throw new IllegalStateException(
+                        "Сервис статистики '" + statsServiceId + "' не найден в Eureka"
+                );
+            }
+
+            ServiceInstance instance = instances.get(0);
+            log.debug("Найден экземпляр: {}:{}", instance.getHost(), instance.getPort());
+            return instance;
+        });
+    }
+
+    private URI buildStatsServiceUri(String path) {
+        ServiceInstance instance = getStatsServiceInstance();
+        return UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host(instance.getHost())
+                .port(instance.getPort())
+                .path(path)
+                .build()
+                .toUri();
     }
 
     public ResponseEntity<Object> saveHit(EndpointHit hit) {
         try {
             log.info("Сохранение информации о запросе {}", hit);
-            return makeResult(restClient.post()
-                    .uri("/hit")
+            URI uri = buildStatsServiceUri("/hit");
+
+            return restClient.post()
+                    .uri(uri)
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(hit)
                     .retrieve()
-                    .body(EndpointHit.class), HttpStatus.CREATED);
+                    .toEntity(Object.class);
+
         } catch (Exception e) {
-            String msg = "Oшибка при сохранении информации";
-            log.error(msg + " {}", e.getMessage(), e);
-            return makeResult(ApiError.builder()
-                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+            log.error("Ошибка при сохранении информации: {}", e.getMessage(), e);
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Collections.singletonMap("error", e.getMessage()));
         }
     }
 
@@ -62,8 +112,9 @@ public class StatClient extends ResponseGenerator {
 
         try {
             log.info("Запрос статистики {}", request);
+            URI baseUri = buildStatsServiceUri("/stats");
 
-            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(statServiceUrl + "/stats")
+            UriComponentsBuilder builder = UriComponentsBuilder.fromUri(baseUri)
                     .queryParam("start", request.getStart().format(FORMATTER))
                     .queryParam("end", request.getEnd().format(FORMATTER))
                     .queryParam("unique", request.getUnique());
@@ -73,23 +124,25 @@ public class StatClient extends ResponseGenerator {
                 builder.queryParam("uris", uris);
             }
 
+            URI finalUri = builder.build().toUri();
+
             ResponseEntity<List<ViewStatDto>> response = restClient.get()
-                    .uri(builder.build().toUri())
+                    .uri(finalUri)
                     .accept(MediaType.APPLICATION_JSON)
                     .retrieve()
                     .toEntity(new ParameterizedTypeReference<>() {
                     });
 
-            if (response.getStatusCode() == HttpStatus.OK) {
+            if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
                 return response.getBody();
             } else {
                 log.error("Ошибка при получении статистики: {}", response.getStatusCode());
-                return Collections.emptyList(); // Или выбросить исключение, в зависимости от требований
+                return Collections.emptyList();
             }
 
         } catch (Exception e) {
-            log.error("Ошибка при запросе статистики: {}", e.getMessage());
-            return Collections.emptyList(); // Или выбросить исключение, в зависимости от требований
+            log.error("Ошибка при запросе статистики: {}", e.getMessage(), e);
+            return Collections.emptyList();
         }
     }
 }
